d33481166d4280643388c1714feb5e75dca2aae7
mediana105 <dizalilova@edu.hse.ru>
2026-03-15 00:30:21 +0300

    add joint optimizer

---
+++ src/optimization/joint_optimizer.py
@@ -0,0 +1,60 @@
+from typing import List, Optional, Dict, Any
+
+from .optimization_algorithms import SimulatedAnnealingOptimizer
+from ..pipeline.mission_result import MissionResult
+
+
+class JointOptimizer:
+    def __init__(self, mission_optimizer):
+        self.mission_optimizer = mission_optimizer
+        self.n_drones = mission_optimizer.num_drones
+        self._last_result: Optional[MissionResult] = None
+        self._last_portions: Optional[List[float]] = None
+
+    def objective(self, portions: List[float]) -> float:
+        result = self.mission_optimizer.evaluate(portions)
+        self._last_result = result
+        self._last_portions = portions
+        return result.mission_time
+
+    def optimize(
+            self,
+            initial_portions: List[float] = None,
+            initial_temp: float = 1000.0,
+            final_temp: float = 1e-10,
+            cooling_rate: float = 0.95,
+            step_size: float = 0.1,
+            max_iterations: int = 1000,
+            seed: Optional[int] = None,
+    ) -> Dict[str, Any]:
+        sa_optimizer = SimulatedAnnealingOptimizer(
+            initial_temp=initial_temp,
+            final_temp=final_temp,
+            cooling_rate=cooling_rate,
+            step_size=step_size,
+            random_seed=seed
+        )
+
+        opt_result = sa_optimizer.optimize(
+            objective_function=self.objective,
+            initial_portions=initial_portions,
+            max_iterations=max_iterations
+        )
+
+        self._last_portions = opt_result['optimized_portions']
+        self._last_result = self.mission_optimizer.evaluate(self._last_portions)
+
+        return {
+            'optimized_portions': opt_result['optimized_portions'],
+            'best_time': opt_result['best_value'],
+            'iterations': opt_result['iterations'],
+            'final_temperature': opt_result['final_temperature'],
+            'success': opt_result['success'],
+            'final_mission_result': self._last_result
+        }
+
+    def get_last_portions(self) -> Optional[List[float]]:
+        return self._last_portions
+
+    def get_last_mission_result(self) -> Optional[MissionResult]:
+        return self._last_result
